b06542a0d26996baf8e861d8c52fb990e6a4bc36
sergey-prochukhan <sergeyprochuhan@gmail.com>
2026-01-08 20:43:40 +0300

    Rename main to order

--- handlers/main_handlers.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, types, F
-from aiogram.filters.command import Command
-from aiogram.types import FSInputFile 
-import os
-
-
-main_han_router = Router()
-
-#@main_han_router
---
+++ handlers/order_handlers.py
@@ -0,0 +1,9 @@
+from aiogram import Router, types, F
+from aiogram.filters.command import Command
+from aiogram.types import FSInputFile 
+import os
+
+
+order_han_router = Router()
+
+#@main_han_router
