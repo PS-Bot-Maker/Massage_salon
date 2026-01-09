cc684625eca1950b83ac068928e6a43df1253f05
PS-Bot-Maker <sergeyprochuhan@gmail.com>
2026-01-09 15:50:23 +0300

    fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 #from dotenv import load_dotenv
 #from handlers.order_handlers import order_han_router
-from handlers.menu_handlers import menu_han_router
+from handlers.menu_handlers import menu_han_router, order_han_router
 
 #load_dotenv()
 logging.basicConfig(level=logging.INFO)
